9417e8fa2a3e1bbb2b7d1568c701c7cb30b4c48f
Taggart Smith <taggart.smith@gmail.com>
2025-03-28 10:17:18 -0600

    Department Services added and now displays

--- src/app/components/departments/departments.component.ts
+++ src/app/components/departments/departments.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DepartmentsService } from '../../services/departments.service';
+import { Department } from '../../interfaces/department';
 
 @Component({
   selector: 'app-departments',
   standalone: false,
   templateUrl: './departments.component.html',
-  styleUrl: './departments.component.scss'
+  styleUrl: './departments.component.scss',
 })
-export class DepartmentsComponent {
-
+export class DepartmentsComponent implements OnInit {
+  departments: Department[] | undefined;
+  constructor(private departmentsServices: DepartmentsService) {}
+  ngOnInit(): void {
+    this.departments = this.departmentsServices.departments;
+  }
 }
