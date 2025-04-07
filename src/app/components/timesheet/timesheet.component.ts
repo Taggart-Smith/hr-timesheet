99516285879ad1f7be1c4344daf74b6edf13fd83
Taggart Smith <taggart.smith@gmail.com>
2025-04-07 09:44:19 -0600

    TimeSheet Component complete

--- src/app/components/timesheet/timesheet.component.ts
+++ src/app/components/timesheet/timesheet.component.ts
@@ -17,6 +17,15 @@ export class TimesheetComponent implements OnInit {
   employeeNameFC = new FormControl('', this.nameValidator());
   employees: Employee[] = [];
   employeeId = 0;
+  weekdays: string[] = [
+    'monday',
+    'tuesday',
+    'wednesday',
+    'thursday',
+    'friday',
+    'saturday',
+    'sunday',
+  ];
 
   constructor(
     private route: ActivatedRoute,
@@ -39,11 +48,19 @@ export class TimesheetComponent implements OnInit {
         departmentId: this.department?.id,
         name: this.employeeNameFC.value,
         payRate: Math.floor(Math.random() * 50) + 50,
+        monday: 0,
+        tuesday: 0,
+        wednesday: 0,
+        thursday: 0,
+        friday: 0,
+        saturday: 0,
+        sunday: 0,
       });
 
       this.employeeNameFC.setValue('');
     }
   }
+
   nameValidator(): ValidatorFn {
     return (control: AbstractControl): { [key: string]: any } | null => {
       let error = null;
@@ -56,5 +73,21 @@ export class TimesheetComponent implements OnInit {
       }
       return error;
     };
+  }
+
+  getTotalHours(employee: Employee): number {
+    return (
+      employee.monday +
+      employee.tuesday +
+      employee.wednesday +
+      employee.thursday +
+      employee.friday +
+      employee.saturday +
+      employee.sunday
+    );
+  }
+
+  deleteEmployee(index: number): void {
+    this.employees.splice(index, 1);
   }
 }
