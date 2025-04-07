0928022354f30b23a9ef1525d8a04bb632e3d76d
Taggart Smith <taggart.smith@gmail.com>
2025-04-07 08:33:01 -0600

    Departments and Timesheet component displays

--- src/app/components/departments/departments.component.ts
+++ src/app/components/departments/departments.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DepartmentsService } from '../../services/departments.service';
 import { Department } from '../../interfaces/department';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-departments',
@@ -10,8 +11,14 @@ import { Department } from '../../interfaces/department';
 })
 export class DepartmentsComponent implements OnInit {
   departments: Department[] | undefined;
-  constructor(private departmentsServices: DepartmentsService) {}
+  constructor(
+    private departmentsServices: DepartmentsService,
+    private router: Router
+  ) {}
   ngOnInit(): void {
     this.departments = this.departmentsServices.departments;
+  }
+  gotoDepartment(departmentId: string): void {
+    this.router.navigate(['./timesheet', { id: departmentId }]);
   }
 }
--- src/app/components/timesheet/timesheet.component.ts
+++ src/app/components/timesheet/timesheet.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Department } from '../../interfaces/department';
+import { DepartmentsService } from '../../services/departments.service';
 
 @Component({
   selector: 'app-timesheet',
   standalone: false,
   templateUrl: './timesheet.component.html',
-  styleUrl: './timesheet.component.scss'
+  styleUrl: './timesheet.component.scss',
 })
-export class TimesheetComponent {
+export class TimesheetComponent implements OnInit {
+  department: Department | undefined;
+  departments: Department[] | undefined;
+  constructor(
+    private route: ActivatedRoute,
+    private departmentsService: DepartmentsService
+  ) {}
 
+  ngOnInit(): void {
+    this.departments = this.departmentsService.departments;
+    this.department = this.departments.find(
+      (department) => department.id === this.route.snapshot.params['id']
+    );
+  }
 }
